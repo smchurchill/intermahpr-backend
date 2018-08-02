03c0b6878ed531a75ed1ba69f0fd9906aedc1aec
Samuel Churchill <meadowschurchill@gmail.com>
2018-08-02 13:26:28 -0700

    Removed grouping information retention due to human-observable performance differences in shiny app.

--- R/utils-mm.R
+++ R/utils-mm.R
@@ -20,5 +20,6 @@ collapseDeprecated <- function(.data) {
   .data %>%
     mutate(im = substring(text = im, first = 1, last = 7)) %>%
     group_by(region, year, gender, age_group, im, outcome) %>%
-    summarise(count = sum(count))
+    summarise(count = sum(count)) %>%
+    ungroup()
 }
